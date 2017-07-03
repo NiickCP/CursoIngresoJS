84a5be1f6465fdd65e00ff3fca0f2f0d549d3495
LuqiMarino <lucasmarino96@hotmail.com>
2017-07-03 10:32:34 -0300

    Add files via upload

---
+++ 3-InstruccionSwitch/Switch 5.js
@@ -0,0 +1,16 @@
+function Mostrar() 
+{
+    var hora = document.getElementById("hora").value;
+
+    switch (hora)
+    {
+        case "7":
+        case "8":
+        case "9":
+        case "10":
+        case "11":
+        alert ("es de mañana");
+        break;
+        
+    }
+}
