fa8905a0447c8d777c4f05a876f0bdb6d4a5c344
LuqiMarino <lucasmarino96@hotmail.com>
2017-07-03 10:11:45 -0300

    Add files via upload

---
+++ 3-InstruccionSwitch/Switch 1.js
@@ -0,0 +1,21 @@
+function Mostrar() 
+{
+    var mes;
+    var mes = document.getElementById("mes").value;
+
+    switch (mes)
+    {
+        case "Enero":
+        alert ("que comiences bien el año");
+        break;
+        case "Marzo":
+        alert ("a clases");
+        break;
+        case "Julio":
+        alert ("se vienen las vacaciones");
+        break;
+        case "Diciembre":
+        alert ("Felices fiestas");
+        break;
+    } 
+}
