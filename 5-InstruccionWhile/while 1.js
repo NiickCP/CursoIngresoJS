1eed93390296c9ad2f5308f542edb6ade04ab3f6
LuqiMarino <lucasmarino96@hotmail.com>
2017-07-05 11:33:21 -0300

    Add files via upload

---
+++ 5-InstruccionWhile/while 1.js
@@ -0,0 +1,9 @@
+var num=1;
+function Mostrar()
+{
+while (num <= 10)
+{
+    alert (num);
+    num = num + 1 ;
+}
+}
---
+++ 5-InstruccionWhile/while 2.js
@@ -0,0 +1,9 @@
+var num=10;
+function Mostrar()
+{
+while (num >= 1)
+{
+    alert (num);
+    num = num - 1;
+}
+}
