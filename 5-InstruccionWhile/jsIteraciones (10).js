7e4443bea351975441983791d5916fff261e642d
Luqimarino <lucasmarino96@hotmail.com>
2017-07-10 12:17:32 -0300

    //asd

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -1,18 +1,47 @@
 function Mostrar()
 {
+	var contpos=0;
+	var contneg=0;
+	var contcer=0;
+	var contpar=0;
+	var num;
+	var respuesta=true;
+	var acumneg=0;
+	var acumpos=0;
+	var rta = true;
 
-	var contador=0;
-	//declarar contadores y variables 
-	
-	var respuesta="si";
-
-	while(respuesta!="no")
+	while(respuesta==true)
 	{
-		
-	
+		num=prompt("Ingrese un numero");
+		num=parseInt(num);
+			if(num<0)
+			{
+				acumneg+=num;
+				contneg+=1;
+			}
+				if(num>0)
+				{
+					acumpos+=num;
+					contpos+=1;
+				}
+					if(num=0)
+					{
+						contcer+=1;
+					}
+						if ((num % 2) = 0)
+						{
+							contpar+=1;
+						}
+		rta = confirm ("¿Desea ingresar otro valor?");
 	}
+	alert ("La suma de los negativos es: "+acumneg);
+	alert ("La suma de los positivos es: "+acumpos);
+	alert ("La cantidad de positivos es: "+contpos);
+	alert ("La cantidad de negativos es: "+contneg);
+	alert ("La cantidad de ceros es: "+contcer);
+	alert ("La cantidad de numeros pares es: "+contpar);
+	alert ("El promedio de los positivos es: "+(acumpos/contpos));
+	alert ("El promedio de los negativos es: "+(acumneg/contneg));
+	alert ("La diferencia entre positivos y negativos es: "+(acumpos-acumneg));
 
-
-
-
-}//FIN DE LA FUNCIÓN
+}
--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -1,18 +1,34 @@
-function Mostrar()
+function Mostrar ()
 {
+    var num;
+    var max;
+    var min;
+    var primera = true;
+    var rta = true;
 
-	var contador=0;
-	// declarar variables
-	
-	var respuesta='si';
-
-	while(respuesta!='no')
-	{
-		
-	
-	}
-
-
-
-
-}//FIN DE LA FUNCIÓN
+    while (rta == true)
+    {
+       num = prompt ("Ingrese un numero");
+       if (primera)
+       {
+           max=num;
+           min=num;
+           primera=false;
+       }
+       else 
+       {
+           if (num < min)
+       {
+           min=num;
+       }
+            if (num > max)
+       {
+           max=num;
+       }
+       }
+       
+        rta = confirm ("¿Quiere seguir ingresando?");
+    }
+    alert ("El maximo es: "+max);
+    alert ("El minimo es: "+min);
+}
