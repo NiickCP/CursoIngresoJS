202682548ed2de87ce79518aa2bc55b5ce50052d
NiickCP <36265129+NiickCP@users.noreply.github.com>
2018-02-15 09:42:38 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -1,8 +1,11 @@
 function Mostrar()
 {
-//tomo la edad  
-
-	
-
-
-}//FIN DE LA FUNCIÓN
+var edad;
+var estado;
+edad=document.getElementById("edad").value;
+estado=document.getElementById("estadoCivil").value;
+if(edad<18 && estado!="Soltero")
+{
+alert("Es muy pequeño para NO ser soltero");
+}	
+}
